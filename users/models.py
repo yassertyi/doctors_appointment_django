052a7ee28a2d92f48f91dd7b3b189f2083b7e82c
Ahmed Algarbani <128222206+ahmedalgarbani@users.noreply.github.com>
2024-11-24 23:23:33 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+#----ahmed----develop
 class User(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
