d28c1cb18baa0c8ca79b133fa92e816dd67b7327
MotasamAhmad <motasam735336099@gmail.com>
2024-12-14 23:38:43 +0300

    update users

--- users/views.py
+++ users/views.py
@@ -1,41 +1,37 @@
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
 from django.contrib.auth.hashers import make_password
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.contrib.auth import login
 from .models import CustomUser
-from . import views
 
 
 class LoginView(auth_views.LoginView):
-    template_name = 'frontend\auth\login.html'
+    template_name = 'frontend/auth/login.html'
 
     def get_success_url(self):
-        return reverse_lazy('frontend\dashboard\patient\index.html')
+        return reverse_lazy('users:dashboard')
 
 
 class LogoutView(auth_views.LogoutView):
     next_page = 'login'
 
 
-class IndexView(TemplateView):
-    template_name = 'frontend\home\index.html'
-
-
 def patient_signup(request):
     if request.method == "POST":
         name = request.POST.get('name')
+        email = request.POST.get('email')
         mobile_number = request.POST.get('mobile_number')
         password = make_password(request.POST.get('password'))
 
         # تخزين بيانات الجلسة
         request.session['name'] = name
+        request.session['email'] = email
         request.session['mobile_number'] = mobile_number
         request.session['password'] = password
-        
+
         return redirect('users:register_step1')
     return render(request, 'frontend/auth/patient-signup.html')
 
@@ -46,14 +42,14 @@ def register_step1(request):
         if profile_picture:
             path = default_storage.save(f'uploads/profile_pictures/{profile_picture.name}', ContentFile(profile_picture.read()))
             request.session['profile_picture'] = path
-            return redirect('users:register_step2')
+        return redirect('users:register_step2')
     return render(request, 'frontend/auth/patient-register-step1.html')
 
 
 def register_step2(request):
     if request.method == "POST":
         gender = request.POST.get('gender')
-        is_pregnant = request.POST.get('is_pregnant') == '1'
+        is_pregnant = request.POST.get('is_pregnant') == 'on'
         pregnancy_term = request.POST.get('pregnancy_term')
         weight = request.POST.get('weight')
         height = request.POST.get('height')
@@ -69,7 +65,7 @@ def register_step2(request):
             'age': age,
             'blood_group': blood_group,
         }
-        return redirect('register_step3')
+        return redirect('users:register_step3')
     return render(request, 'frontend/auth/patient-register-step2.html')
 
 
@@ -83,7 +79,7 @@ def register_step3(request):
             'father': request.POST.get('father') == '1',
         }
         request.session['family_data'] = family_data
-        return redirect('register_step4')
+        return redirect('users:register_step4')
     return render(request, 'frontend/auth/patient-register-step3.html')
 
 
@@ -94,7 +90,7 @@ def register_step4(request):
 
         request.session['city'] = city
         request.session['state'] = state
-        return redirect('register_step5')
+        return redirect('users:register_step5')
     return render(request, 'frontend/auth/patient-register-step4.html')
 
 
@@ -103,6 +99,7 @@ def register_step5(request):
         # حفظ بيانات المستخدم النهائية
         user = CustomUser(
             username=request.session['name'],
+            email=request.session['email'],
             mobile_number=request.session['mobile_number'],
             password=request.session['password'],
             profile_picture=request.session.get('profile_picture', None),
@@ -116,12 +113,12 @@ def register_step5(request):
             family_data=request.session['family_data'],
             city=request.session['city'],
             state=request.session['state'],
+            user_type='patient'  # تحديد نوع المستخدم كمريض
         )
         user.save()
         login(request, user)
-        return redirect('dashboard')
-    return render(request, 'frontend/auth/patient-register-step4.html')
-
+        return redirect('users:dashboard')
+    return render(request, 'frontend/auth/patient-register-step5.html')
 
 def dashboard(request):
     return render(request, 'frontend/dashboard/patient/index.html', {'user': request.user})
