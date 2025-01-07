64eccad8bf41e324c01a38bd92e896b58f5ac607
yasser <you@yasser.com>
2025-01-07 21:16:49 +0300

    اضافة الاشعارات للمريض

--- patients/urls.py
+++ patients/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import appointments_dashboard, patients_list, patient_dashboard
+from .views import patient_dashboard
 
 app_name = 'patients'
 
 urlpatterns = [
-    path('patients/', appointments_dashboard, name='appointments_dashboard'),
-    path('', patients_list, name='patients_list'),
-    path('<int:patient_id>/', patient_dashboard, name='patient_dashboard'),
+    path('', patient_dashboard, name='patient_dashboard'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -3,41 +3,22 @@ from .models import Patients, Favourites
 from bookings.models import Booking
 from doctors.models import Doctor
 from reviews.models import Review
+from notifications.models import Notifications
 from django.db.models import Avg, Prefetch
 from datetime import datetime
-from django.db.models import Avg, Count
 
+def patient_dashboard(request):
+    user_id = 1
+    patient = get_object_or_404(Patients, user_id=user_id)
 
-def appointments_dashboard(request):
-    patients = Patients.objects.all()
-    favourites = Favourites.objects.filter(user=request.user) if request.user.is_authenticated else None
-    
-    context = {
-        'patients': patients,
-        'favourites': favourites,
-    }
-    return render(request, 'frontend/dashboard/doctor/index.html', context)
+    notifications = get_notifications_for_user(user_id=user_id)
 
-
-def patients_list(request):
-    # جلب قائمة جميع المرضى
-    patients = Patients.objects.all()
-    context = {
-        'patients': patients,
-    }
-    return render(request, 'frontend/dashboard/patient/sections/patient_lite.html', context)
-
-
-from django.db.models import Avg
-
-def patient_dashboard(request, patient_id):
-    patient = get_object_or_404(Patients, id=patient_id)
+    unread_notifications_count = notifications.filter(status='0').count()
 
     favourite_doctors, ratings_context = get_favourites_and_ratings(patient)
-
     for doctor in favourite_doctors:
         average_rating = doctor.reviews.aggregate(Avg('rating'))['rating__avg']
-        doctor.average_rating = average_rating if average_rating is not None else 0  
+        doctor.average_rating = average_rating if average_rating is not None else 0
 
     if request.method == 'POST':
         update_patient_data(patient, request)
@@ -47,11 +28,12 @@ def patient_dashboard(request, patient_id):
         'favourites': Favourites.objects.filter(patient=patient),
         'bookings': Booking.objects.filter(patient=patient),
         'favourite_doctors': favourite_doctors,
-        'ratings_context': ratings_context,  
+        'ratings_context': ratings_context,
+        'notifications': notifications, 
+        'unread_notifications_count': unread_notifications_count,  
     }
 
     return render(request, 'frontend/dashboard/patient/index.html', context)
-
 
 def update_patient_data(patient, request):
     """
@@ -75,26 +57,24 @@ def update_patient_data(patient, request):
     if request.FILES.get('profile_picture'):
         patient.profile_picture = request.FILES['profile_picture']
 
-    # حفظ التغييرات
     patient.save()
 
-
-
 def get_favourites_and_ratings(patient):
+    """
+    دالة لجلب الأطباء المفضلين وحساب تقييماتهم.
+    """
     favourites = Favourites.objects.filter(patient=patient)
     favourite_doctors = [favourite.doctor for favourite in favourites]
     
-    # Get doctors with their reviews
     doctors_with_reviews = Doctor.objects.prefetch_related(
         Prefetch('reviews', queryset=Review.objects.all(), to_attr='doctor_reviews')
     ).filter(id__in=[doctor.id for doctor in favourite_doctors])
     
-    # Calculate average ratings for each doctor
+    # حساب متوسط التقييمات لكل طبيب
     ratings_context = {}
     for doctor in doctors_with_reviews:
         reviews = doctor.doctor_reviews
         if reviews:
-            # Calculate average rating
             total_rating = sum(review.rating for review in reviews)
             average_rating = total_rating / len(reviews)
         else:
@@ -102,3 +82,12 @@ def get_favourites_and_ratings(patient):
         ratings_context[str(doctor.id)] = average_rating
 
     return favourite_doctors, ratings_context
+
+
+
+def get_notifications_for_user(user_id):
+    """
+    دالة لجلب الإشعارات الخاصة بالمستخدم الذي يمتلك id معين.
+    """
+    notifications = Notifications.objects.filter(user_id=user_id, is_active=True).order_by('-send_time')
+    return notifications
